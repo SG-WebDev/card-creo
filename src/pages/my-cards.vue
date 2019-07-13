65d4b7e588342c436cdba7baec55034e164f4a86
SG-WebDev <sgrobelny.contact@gmail.com>
2019-07-14 01:38:14 +0200

    swiper with delete function

--- src/pages/my-cards.vue
+++ src/pages/my-cards.vue
@@ -5,12 +5,30 @@
                     v-for="card in cards"
                     :key="card._id"
                     :title="card.name"
+                    swipeout
+                    @swipeout:deleted="deleteCard(card._id)"
                     :link="`/my-cards/${card._id}/`"
             >
+                <f7-swipeout-actions right>
+                    <f7-swipeout-button delete confirm-text="Are you sure you want to delete this card?">Delete</f7-swipeout-button>
+                </f7-swipeout-actions>
             </f7-list-item>
         </f7-list>
     </f7-page>
 </template>
+<style lang="scss">
+.swipeout {
+    .item-content {
+        padding-left: 0;
+        .item-inner {
+            padding: 20px;
+            &::before {
+                color: #ec644b;
+            }
+        }
+    }
+}
+</style>
 <script>
     import {HTTP} from '../js/httpBase';
     export default {
@@ -34,6 +52,17 @@
             else {
                 this.$f7.dialog.alert(`Something went wrong!`);
             }
+        },
+        methods : {
+          deleteCard: function (e) {
+              HTTP.delete(`api/card/${e}`)
+                  .then(response => {
+                      this.$f7.dialog.alert('Card has been removed!');
+                  })
+                  .catch(e => {
+                      this.$f7.dialog.alert(`Database is not responding. Try again later`);
+                  });
+          }
         },
     };
 </script>
